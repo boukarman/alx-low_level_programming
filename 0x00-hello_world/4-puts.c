a5171839d4409ef740e773eb7e42aa5f12597359
boukarman <6384079+boukarman@users.noreply.github.com>
2022-12-09 17:27:51 -0800

    task completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point 
+ *
+ * Description : prints "Programming is like builduing a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("\"programming is like builduing a multilingual puzzle");
+	return(0);
+}
