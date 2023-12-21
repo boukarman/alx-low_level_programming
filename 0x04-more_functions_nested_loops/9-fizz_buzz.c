05cc9486fd9161e0046fd76b2da9bba25da918d0
Naoufel Boukarma <boukarman@yahoo.fr>
2023-12-21 14:58:55 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,11 +3,9 @@
 
 /**
  * main - entry point
- *
  * Description: prints the numbers 1 - 100 
  * Fizz for multiples of 3, Buzz for multiples of 5
  * and FizzBuzz for multiples of both
- *
  * Return: always 0
  */
 int main(void)
