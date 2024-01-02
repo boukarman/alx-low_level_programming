0c047f59d4436f2fae415386ee03e9d6418223a4
Naoufel Boukarma <boukarman@yahoo.fr>
2024-01-02 14:44:47 -0800

    value pointer update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset  to 98
+ * @n: value
+ *
+ * Description: takes a pointer as a param and update it to 98
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
