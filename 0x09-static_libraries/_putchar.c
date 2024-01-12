8da7aaf13aea08952bda6a8dbdd4a74549832b62
Naoufel Boukarma <boukarman@yahoo.fr>
2024-01-12 13:27:53 -0800

    static libraries

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+
+
+/**
+ *
+ *	_putchar - writes the character c in the output
+ *
+ * 	@c: The character to print
+ *
+ *	Return: On success 1 and -1 if error
+ */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
