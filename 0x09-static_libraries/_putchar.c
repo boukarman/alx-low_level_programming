0a7181bb1f6dfc338fd8373d6b5fa2e9b142f4ea
Naoufel Boukarma <boukarman@yahoo.fr>
2024-01-12 13:40:32 -0800

    stati libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,12 +4,15 @@
 
 /**
  *
- * _putchar - writes the character c in the output
+ *  * _putchar - writes the character c to stdout
  *
- * @c: The character to print
+ *   * @c: The character to print
  *
- * Return: On success 1 and -1 if error
- */
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately*/
 
 int _putchar(char c)
 
