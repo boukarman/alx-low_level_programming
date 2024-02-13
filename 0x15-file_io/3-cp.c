d361e01577f9daeb16cec0efe75e994a2d203017
Naoufel Boukarma <boukarman@yahoo.fr>
2024-02-12 16:12:34 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,21 +20,21 @@ void check_97(int argc)
  * check_98 - check that file descirptor was closed
  * @check: check if true or not
  * @file: file
- * @fd_from: file descriptor from
- * @fd_to: file desciroptor to
+ * @fdes_from: file descriptor from
+ * @fdes_to: file desciroptor to
  *
  * Return: nothing
  */
 
-void check_98(ssize_t check, char *file, int fd_from, int fd_to)
+void check_98(ssize_t check, char *file, int fdes_from, int fdes_to)
 {
 	if (check == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		if (fd_from != -1)
-			close(fd_from);
-		if (fd_to != -1)
-			close(fd_to);
+		if (fdes_from != -1)
+			close(fdes_from);
+		if (fdes_to != -1)
+			close(fdes_to);
 		exit(98);
 	}
 }
@@ -42,35 +42,35 @@ void check_98(ssize_t check, char *file, int fd_from, int fd_to)
  * check_99 - check that file descirptors was closed
  * @check: check if true or not
  * @file: file
- * @fd_from: file descriptor from
- * @fd_to: file descirptor to
+ * @fdes_from: file descriptor from
+ * @fdes_to: file descirptor to
  *
  * Return: nothing
  */
-void check_99(ssize_t check, char *file, int fd_from, int fd_to)
+void check_99(ssize_t check, char *file, int fdes_from, int fdes_to)
 {
 	if (check == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		if (fd_from != -1)
-			close(fd_from);
-		if (fd_to != -1)
-			close(fd_to);
+		if (fdes_from != -1)
+			close(fdes_from);
+		if (fdes_to != -1)
+			close(fdes_to);
 		exit(99);
 	}
 }
 /**
  * check_100 - check that file descirptor was closed
  * @check: check if true or not
- * @f_d: file descriptor
+ * @f_des: file descriptor
  *
  * Return: nothing
  */
-void check_100(int check, int f_d)
+void check_100(int check, int f_des)
 {
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_d);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_des);
 		exit(100);
 	}
 }
@@ -83,28 +83,28 @@ void check_100(int check, int f_d)
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, close_to, close_from;
+	int fdes_from, fdes_to, close_to, close_from;
 	ssize_t len_r, len_w;
 	char buffer[BUFFER_SIZE];
 
 	check_97(argc);
-	fd_from = open(argv[1], O_RDONLY);
-	check_98((ssize_t)fd_from, argv[1], -1, -1);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	check_99((ssize_t)f_to, argv[2], fd_from, -1);
+	fdes_from = open(argv[1], O_RDONLY);
+	check_98((ssize_t)fdes_from, argv[1], -1, -1);
+	fdes_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	check_99((ssize_t)fdes_to, argv[2], fdes_from, -1);
 	len_r = BUFFER_SIZE;
 	while (len_r == BUFFER_SIZE)
 	{
 		len_r = read(fd_from, buffer, BUFFER_SIZE);
-		check_98(len_r, argv[1], fd_from, fd_to);
+		check_98(len_r, argv[1], fdes_from, fdes_to);
 		lenw = write(fd_to, buffer, lenr);
 		if (len_w != len_r)
 			len_w = -1;
-		check_99(len_w, argv[2], fd_from, fd_to);
+		check_99(len_w, argv[2], fdes_from, fdes_to);
 	}
-	close_to = close(f_to);
-	close_from = close(fd_from);
-	check_100(close_to, fd_to);
-	check_100(close_from, fd_from);
+	close_to = close(fdes_to);
+	close_from = close(fdes_from);
+	check_100(close_to, fdes_to);
+	check_100(close_from, fdes_from);
 	return (0);
 }
