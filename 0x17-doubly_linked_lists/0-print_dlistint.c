5b33e884f975a523dae108a42b9943c8e94d2f26
boukarmann <boukarman@gmail.com>
2024-03-21 15:47:04 -0700

    print distinct

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all elements of a doubly linked list
+ * @h: head
+ *
+ * Return: size of doubly linkded list or 0 if empty
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
