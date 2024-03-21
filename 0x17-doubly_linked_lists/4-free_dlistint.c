067a6473af8a34416d633c2d98a05238abdea0e2
boukarmann <boukarman@gmail.com>
2024-03-21 16:02:50 -0700

    free node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the doubly linked list
+ * @head: head
+ *
+ * Return: nothing
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = NULL;
+
+	while (head != NULL)
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
+}
